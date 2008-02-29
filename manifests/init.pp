5076f8a68d1e17578d2d830c48a77d57ef9613e3
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-02-29 11:00:18 +0000

    notice not warn
    git-svn-id: https://svn/ipuppet/trunk/modules/sshd@893 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,7 +52,7 @@ define sshd::sshd_config(
 		default => $source,
 	}
 
-    warn("sshd_allowed_users is set to ${sshd_allowed_users}")
+    notice("sshd_allowed_users is set to ${sshd_allowed_users}")
 
     $real_sshd_allowed_users = $sshd_allowed_users ? {
         ''  => 'root',
