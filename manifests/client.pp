1afa887a2e99bc97ebae388d74185ec4e11a38ed
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-09-29 22:45:39 +0000

    factored out the package to some subclasses as openbsd doesn't need such a package
    git-svn-id: https://svn/ipuppet/trunk/modules/sshd@2265 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/client.pp
+++ manifests/client.pp
@@ -3,15 +3,16 @@
 class sshd::client {
     case $operatingsystem {
         debian: { include sshd::client::debian }
-        default: { include sshd::client::base }
+        default: { 
+            case $kernel {
+                linux: { include sshd::client::linux }
+                default:  { include sshd::client::base }
+            }
+        }
     }
 }
 
 class sshd::client::base {
-    package {'openssh-clients':
-        ensure => installed,
-    }
-
     # this is needed because the gid might have changed
     file { '/etc/ssh/ssh_known_hosts':
             mode => 0644, owner => root, group => 0;
@@ -19,6 +20,12 @@ class sshd::client::base {
     
     # Now collect all server keys
     Sshkey <<||>>
+}
+
+class sshd::client::linux inherits sshd::client::base {
+    package {'openssh-clients':
+        ensure => installed,
+    }
 }
 
 class sshd::client::debian inherits sshd::client::base {
