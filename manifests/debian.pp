384b1466ea7b860f7f17b05281c49fa5a8ebe42a
mh <mh@immerda.ch>
2009-10-30 22:07:23 +0100

    switch to new lsb way

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,13 +1,16 @@
 class sshd::debian inherits sshd::linux {
 
   # the templates for Debian need lsbdistcodename
-  include assert_lsbdistcodename
+  include lsb
+  File['sshd_config']{
+    require => Package['lsb']
+  }
 
-    Package[openssh]{
-        name => 'openssh-server',
-    }
-    Service[sshd]{
-        name => 'ssh',
-        hasstatus => false,
-    }
+  Package[openssh]{
+    name => 'openssh-server',
+  }
+  Service[sshd]{
+    name => 'ssh',
+    hasstatus => false,
+  }
 }
