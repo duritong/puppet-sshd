456e42b035e53e72e1aebd4689fa1a230fb49226
intrigeri <intrigeri@boum.org>
2010-10-04 22:03:49 +0200

    Simplify by using the config_file definition.

--- manifests/client/base.pp
+++ manifests/client/base.pp
@@ -1,7 +1,6 @@
 class sshd::client::base {
   # this is needed because the gid might have changed
-  file { '/etc/ssh/ssh_known_hosts':
-    owner => root, group => 0, mode => 0644;
+  config_file { '/etc/ssh/ssh_known_hosts':
   }
 
   # Now collect all server keys
