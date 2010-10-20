eec9e7242525cd9909c0c54cdb7515047bac4407
mh <mh@immerda.ch>
2010-10-20 22:57:15 +0200

    extend sshd::nagios with ensure param

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -1,8 +1,19 @@
 define sshd::nagios(
+  $ensure = 'present',
   $check_hostname = 'absent'
 ) {
   case $check_hostname {
-    'absent': { nagios::service{ "ssh_port_${name}": check_command => "check_ssh_port!$name" } }
-    default: { nagios::service{"ssh_port_host_${name}_${check_hostname}": check_command => "check_ssh_port_host!${name}!${check_hostname}" } }
+    'absent': {
+      nagios::service{"ssh_port_${name}":
+        ensure => $esnure,
+        check_command => "check_ssh_port!$name"
+      }
+    }
+    default: {
+      nagios::service{"ssh_port_host_${name}_${check_hostname}":
+        ensure => $esnure,
+        check_command => "check_ssh_port_host!${name}!${check_hostname}"
+      }
+    }
   }
 }
