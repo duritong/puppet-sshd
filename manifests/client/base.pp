5ce0dcda9776ada2b556f7e1de31702a32a9b318
Tomas Barton <barton.tomas@gmail.com>
2014-01-26 11:33:41 +0100

    client spec

--- manifests/client/base.pp
+++ manifests/client/base.pp
@@ -1,9 +1,10 @@
 class sshd::client::base {
   # this is needed because the gid might have changed
   file { '/etc/ssh/ssh_known_hosts':
-    mode  => '0644',
-    owner => root,
-    group => 0;
+    ensure => present,
+    mode   => '0644',
+    owner  => root,
+    group  => 0;
   }
 
   # Now collect all server keys
