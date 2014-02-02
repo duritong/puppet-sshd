a0e961674b96c070e0a32bce3f224a1512aa128d
Tomas Barton <barton.tomas@gmail.com>
2014-02-02 17:48:24 +0100

    tests for ssh authorized key

---
+++ spec/defines/ssh_authorized_key_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'sshd::ssh_authorized_key' do
+
+  context 'add authorized key' do
+    let(:title) { 'foo' }
+    let(:ssh_key) { 'some_secret_ssh_key' }
+
+    let(:params) {{
+        :key => ssh_key,
+    }}
+
+    it { should contain_ssh_authorized_key('foo').with({
+        'ensure' => 'present',
+        'type'   => 'ssh-dss',
+        'user'   => 'foo',
+        'target' => '/home/foo/.ssh/authorized_keys',
+        'key'    => ssh_key,
+      })
+    }
+  end
+end
