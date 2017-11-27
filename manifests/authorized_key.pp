beb5e5f6c4fd8e6d1de92fd132398c74aab804fb
Micah Anderson <micah@riseup.net>
2017-11-27 15:19:57 -0500

    we still need to have the override_builtin parameter

--- manifests/authorized_key.pp
+++ manifests/authorized_key.pp
@@ -1,11 +1,12 @@
 # wrapper to have some defaults.
 define sshd::authorized_key(
-    $ensure  = 'present',
-    $type    = 'ssh-rsa',
-    $key     = 'absent',
-    $user    = $name,
-    $target  = undef,
-    $options = false,
+  $ensure  = 'present',
+  $type    = 'ssh-rsa',
+  $key     = 'absent',
+  $user    = $name,
+  $target  = undef,
+  $options = false,
+  $override_builtin = undef
 ){
 
   if ($ensure=='present') and ($key=='absent') {
@@ -23,20 +24,54 @@ define sshd::authorized_key(
       $real_target = $target
     }
   }
-  ssh_authorized_key{$name:
-    ensure => $ensure,
-    target => $real_target,
-    user   => $user,
-  }
-  if $ensure == 'present' {
-    Ssh_authorized_key[$name]{
-      type => $type,
-      key  => $key,
+
+  # The ssh_authorized_key built-in function (in 4.8.2 at least) will not write
+  # an authorized_keys file for a mortal user to a directory they don't have
+  # write permission to, puppet attempts to create the file as the user
+  # specified with the user parameter and fails.  Since ssh will refuse to use
+  # authorized_keys files not owned by the user, or in files/directories that
+  # allow other users to write, this behavior is deliberate in order to prevent
+  # typical non-working configurations. However, it also prevents the case of
+  # puppet, running as root, writing a file owned by a mortal user to a common
+  # authorized_keys directory such as one might specify in sshd_config with
+  # something like 'AuthorizedKeysFile /etc/ssh/authorized_keys/%u' So we
+  # provide a way to override the built-in and instead just install via a file
+  # resource. There is no additional security risk here, it's nothing a user
+  # can't already do by writing their own file resources, we still depend on the
+  # filesystem permissions to keep things safe.
+  if $override_builtin {
+    $header = "# HEADER: This file is managed by Puppet.\n"
+
+    if $options == 'absent' {
+      info("not setting any option for ssh_authorized_key: ${name}")
+      $content = "${header}${type} ${key}\n"
+    } else {
+      $content = "${header}${options} ${type} ${key}\n"
     }
 
-    if $options {
+    file { $real_target:
+      ensure  => $ensure,
+      content => $content,
+      owner   => $user,
+      mode    => '0600',
+    }
+
+  } else {
+    ssh_authorized_key { $name:
+      ensure => $ensure,
+      target => $real_target,
+      user   => $user,
+    }
+    if $ensure == 'present' {
       Ssh_authorized_key[$name]{
-        options => $options,
+        type => $type,
+        key  => $key,
+      }
+
+      if $options {
+        Ssh_authorized_key[$name]{
+          options => $options,
+        }
       }
     }
   }
