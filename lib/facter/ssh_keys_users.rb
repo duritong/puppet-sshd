cca80ee6bc879e67335f0dc6329e0bcab80a6c62
Jerome Charaoui <jerome@riseup.net>
2018-01-05 18:04:44 -0500

    structure each key as a type/key/comment hash

--- lib/facter/ssh_keys_users.rb
+++ lib/facter/ssh_keys_users.rb
@@ -11,11 +11,11 @@ Facter.add(:ssh_keys_users) do
       keys = []
       Dir.glob(File.join(user.dir, '.ssh', '*.pub')).each { |filepath|
         if FileTest.file?(filepath)
-          regex = %r{^ssh-\S+ \S+(?:\s+\S+)?$}
+          regex = %r{^ssh-(\S+) (\S+)\s?(.+)?$}
           begin
             line = File.open(filepath).read.chomp
             if (match = regex.match(line))
-              keys += [line]
+              keys += [{ 'type' => match[1], 'key' => match[2], 'comment' => match[3] }]
             end
           rescue
             puts "cannot read user SSH key: " + user.name
