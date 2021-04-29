74c6aa39c61974e1cbc21c47d11dadcf33d08bb7
mh <mh@immerda.ch>
2021-04-29 16:05:09 +0200

    networking.ip fact is now stable

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,15 @@
 # manage an sshd installation
-class sshd(
+class sshd (
   Boolean
-    $manage_nagios = false,
+  $manage_nagios = false,
   $nagios_check_ssh_hostname = 'absent',
   Array[Variant[String,Integer]]
-    $ports = [ 22 ],
+  $ports = [22],
   Boolean
-    $shared_ip = false,
+  $shared_ip = false,
   $ensure_version = 'installed',
   Array[String]
-    $listen_address = [ '0.0.0.0', '::' ],
+  $listen_address = ['0.0.0.0', '::'],
   $allowed_users = '',
   $allowed_groups = '',
   $use_pam = 'no',
@@ -32,12 +32,12 @@ class sshd(
   $rhosts_rsa_authentication = 'no',
   $hostbased_authentication = 'no',
   $permit_empty_passwords = 'no',
-  $authorized_keys_file = $facts['osfamily'] ? {
-    'Debian' => $facts['operatingsystemmajrelease'] ? {
+  $authorized_keys_file = $facts['os']['family'] ? {
+    'Debian' => $facts['os']['release']['major'] ? {
       '6'     => '%h/.ssh/authorized_keys',
       default => '%h/.ssh/authorized_keys %h/.ssh/authorized_keys2',
     },
-    'RedHat' => $facts['operatingsystemmajrelease'] ? {
+    'RedHat' => $facts['os']['release']['major'] ? {
       '5'     => '%h/.ssh/authorized_keys',
       '6'     => '%h/.ssh/authorized_keys',
       default => '%h/.ssh/authorized_keys',
@@ -46,34 +46,33 @@ class sshd(
     default   => '%h/.ssh/authorized_keys %h/.ssh/authorized_keys2',
   },
   Variant[Boolean,String]
-    $hardened = false,
+  $hardened = false,
   Boolean
-    $hardened_client = false,
+  $hardened_client = false,
   Boolean
-    $harden_moduli = false,
+  $harden_moduli = false,
   Boolean
-    $use_host_dsa_key = true,
+  $use_host_dsa_key = true,
   Boolean
-    $use_host_ecdsa_key = false,
+  $use_host_ecdsa_key = false,
   $sftp_subsystem = '',
   $head_additional_options = '',
   $tail_additional_options = '',
   $print_motd = 'yes',
   Array[String[1]]
-    $accept_env = ['LANG', 'LC_*'],
+  $accept_env = ['LANG', 'LC_*'],
   Boolean
-    $manage_firewall = false,
+  $manage_firewall = false,
   $firewall_source = 'net',
-  Stdlib::IP::Address::V4::Nosubnet $sshkey_ipaddress = pick($facts['default_ipaddress'],$facts['ipaddress']),
+  Stdlib::IP::Address::V4::Nosubnet $sshkey_ipaddress = $facts['networking']['ip'],
   Optional[Stdlib::IP::Address::V6::Nosubnet] $sshkey_ip6address = $facts['networking']['ip6'],
   Boolean
-    $manage_client = true,
+  $manage_client = true,
   Boolean
-    $purge_sshkeys = true,
+  $purge_sshkeys = true,
 ) {
-
   if $manage_client {
-    class{'sshd::client':
+    class { 'sshd::client':
       shared_ip       => $shared_ip,
       ensure_version  => $ensure_version,
       manage_firewall => $manage_firewall,
@@ -81,7 +80,7 @@ class sshd(
     }
   }
 
-  case $facts['operatingsystem'] {
+  case $facts['os']['name'] {
     'Gentoo': { include sshd::gentoo }
     'RedHat','CentOS': { include sshd::redhat }
     'OpenBSD': { include sshd::openbsd }
@@ -91,14 +90,14 @@ class sshd(
 
   if $manage_nagios {
     $ports.each |$port| {
-      sshd::nagios{String($port):
+      sshd::nagios { String($port):
         check_hostname => $nagios_check_ssh_hostname,
       }
     }
   }
 
   if $manage_firewall {
-    class{'firewall::rules::ssh':
+    class { 'firewall::rules::ssh':
       ports  => $ports,
       source => $firewall_source,
     }
