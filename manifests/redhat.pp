5ca7cbca85295c58d97716cb71a68e704e9a5a03
mh <mh@immerda.ch>
2021-05-25 15:38:19 +0200

    make sure we only start sshd once network is ready

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -1,5 +1,15 @@
 class sshd::redhat inherits sshd::linux {
-    Package[openssh]{
-        name => 'openssh-server',
+  Package[openssh] {
+    name => 'openssh-server',
+  }
+  # make sure network is here when starting sshd
+  if $sshd::listen_address != ['0.0.0.0', '::'] and versioncmp($facts['os']['release']['major'],'8') >= 0 {
+    systemd::dropin_file {
+      'sshd-wait-on-network':
+        unit     => 'sshd.service',
+        filename => 'wait-on-network.conf',
+        content  => "[Unit]\nWants=network-online.target\nAfter=network-online.target",
+        notify   => Service['sshd'],
     }
+  }
 }
